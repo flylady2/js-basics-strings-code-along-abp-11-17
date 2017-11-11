627bb6c2a41e7cb0eb71dee376a5a2d0c962039a
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-11 02:46:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Ada Lovelace'
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = welcomeMessage.touppercase();
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
